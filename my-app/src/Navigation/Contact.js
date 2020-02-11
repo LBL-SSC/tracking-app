7befa8245eaba0395ec4073f72d1dd4acf607759
rae-glazier <glazier.home.life@gmail.com>
2020-02-11 16:45:11 -0600

    ,

--- my-app/src/Navigation/Contact.js
+++ my-app/src/Navigation/Contact.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ContactForm from '../Forms/ContactForm';
 
-class Contact extends Component {
-
-    render(){
-        
-        return(
-            <div>
-                <h1>Contact</h1>
-                <ContactForm />
-            </div>
-        )
-    }
-    
+const Contact = () => {
+    return(
+        <div>
+            <h1>Contact</h1>
+            <ContactForm />
+        </div>
+    )
 };
 
 export default Contact;
