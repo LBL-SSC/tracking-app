9d34aac319b86e8229c4af9ea1175ee9c3fcaa5e
rae-glazier <glazier.home.life@gmail.com>
2020-02-11 15:57:24 -0600

    building out basic navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Route } from 'react-router-dom';
+
+// components 👇
+import { Home, About, Contact, NavBar } from './Navigation';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <NavBar />
+
+    <Route exact path='/' component={Home} />
+    <Route path='/About' component={About} />
+    <Route path='/Contact' component={Contact} />
+
+  </div>
   );
 }
 
